c74637f81f4073654a017d006dfe2cf895bf3c4a
streetninja22 <streetninja22@gmail.com>
2018-05-12 00:20:01 -0500

    init

---
+++ src/framework/includeSDL.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+//  This header is intended to increase portability, since Xcode/MacOS puts SDL under SDL2/SDL.h, while on Windows/Visual Studio it will simply be SDL.h
+#ifdef __APPLE__
+
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#incldue <SDL2_mixer/SDL_mixer.h>
+#include <SDL2_ttf/SDL_ttf.h>
+
+#else
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+
+#endif
